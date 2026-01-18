a2aac5603988dcda01b2a119f2c4738c4e7dfda0
Marcelo Hadad <marcelo.hadad@outlook.com>
2026-01-18 03:35:55 -0300

    feat: add task1

---
+++ task1/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="hello">
+        <h1>Hello World!</h1>
+        <span>
+          Created with <em>Vite</em>
+        </span>
+      </div>
+    </>
+  );
+}
+
+export default App;
